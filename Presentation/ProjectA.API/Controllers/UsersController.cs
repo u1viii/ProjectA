6f39ec99d293c0a52e48279de87d942378d95597
ulviabdullazada <ulviabdullazada@code.edu.az>
2023-08-23 03:43:16 +0400

    changed appsettings

--- Presentation/ProjectA.API/Controllers/UsersController.cs
+++ Presentation/ProjectA.API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ProjectA.Application.Abstractions.Services;
 using ProjectA.Application.Enums;
 using ProjectA.Application.Features.Commands.AppUsers.CreateUser;
 using ProjectA.Application.Features.Commands.AppUsers.Login;
@@ -12,12 +13,13 @@ namespace ProjectA.API.Controllers
     public class UsersController : ControllerBase
     {
         IMediator _mediator { get; }
-        RoleManager<IdentityRole> _roleManager { get; }
-
-        public UsersController(IMediator mediator, RoleManager<IdentityRole> roleManager)
+        //RoleManager<IdentityRole> _roleManager { get; }
+        IUserService _userService { get; }
+        public UsersController(IMediator mediator, IUserService userService)
         {
             _mediator = mediator;
-            _roleManager = roleManager;
+            //_roleManager = roleManager;
+            _userService = userService;
         }
         [HttpPost]
         public async Task<IActionResult> Create(CreateUserCommandRequest request)
@@ -32,22 +34,28 @@ namespace ProjectA.API.Controllers
             return Ok(response);
         }
         [HttpPost("[action]")]
+        public async Task<IActionResult> GoogleLogin(string token)
+        {
+            await _userService.GoogleAuth(token);
+            return Ok();
+        }
+        [HttpPost("[action]")]
         public async Task<IActionResult> RefreshTokenLogin(LoginCommandRequest request)
         {
             var response = await _mediator.Send(request);
             return Ok(response);
         }
-        [HttpPost("[action]")]
-        public async Task<IActionResult> CreateRoles()
-        {
-            foreach (string item in Enum.GetValues(typeof(UserRoles)))
-            {
-                if (await _roleManager.RoleExistsAsync(item))
-                {
-                    await _roleManager.CreateAsync(new IdentityRole(item));
-                }
-            }
-            return Ok();
-        }
+        //[HttpPost("[action]")]
+        //public async Task<IActionResult> CreateRoles()
+        //{
+        //    foreach (string item in Enum.GetValues(typeof(UserRoles)))
+        //    {
+        //        if (await _roleManager.RoleExistsAsync(item))
+        //        {
+        //            await _roleManager.CreateAsync(new IdentityRole(item));
+        //        }
+        //    }
+        //    return Ok();
+        //}
     }
 }
--- Presentation/ProjectA.API/Extensions/UseConfigureExeptionHandler.cs
+++ Presentation/ProjectA.API/Extensions/UseConfigureExeptionHandler.cs
@@ -4,54 +4,52 @@ using System;
 using System.Net.Mime;
 using System.Text.Json;
 
-namespace ProjectA.API.Extensions
+namespace ProjectA.API.Extensions;
+public static class UseConfigureExeptionHandler
 {
-    public static class UseConfigureExeptionHandler
+    public static void UseConfigureExceptionHandler<T>(this WebApplication app, ILogger<T> logger)
     {
-        public static void UseConfigureExceptionHandler<T>(this WebApplication app, ILogger<T> logger)
+        app.UseExceptionHandler(builder =>
         {
-            app.UseExceptionHandler(builder =>
+            builder.Run(async context =>
             {
-                builder.Run(async context =>
+                var contextFeatures = context.Features.Get<IExceptionHandlerFeature>();
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = MediaTypeNames.Application.Json;
+                if (contextFeatures != null)
                 {
-                    var contextFeatures = context.Features.Get<IExceptionHandlerFeature>();
-                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                    context.Response.ContentType = MediaTypeNames.Application.Json;
-                    if (contextFeatures != null)
+                    //TODO: switch-case`e cevir
+                    int statusCode = 500;
+                    string message = String.Empty;
+                    if (contextFeatures.Error is IBaseException customEx)
                     {
-                        //TODO: switch-case`e cevir
-                        int statusCode = 500;
-                        string message = String.Empty;
-                        if (contextFeatures.Error is IBaseException customEx)
-                        {
-                            statusCode = customEx.StatusCode;
-                            message = customEx.ErrorMessage;
-                        }
-                        else if (contextFeatures.Error is InvalidOperationException)
-                        {
-                            statusCode = 400;
-                            message = contextFeatures.Error.Message ?? contextFeatures.Error.InnerException?.Message ??"Bad Request";
-                        }
-                        else if (contextFeatures.Error is ArgumentNullException)
-                        {
-                            statusCode = 400;
-                            message = contextFeatures.Error.Message ?? contextFeatures.Error.InnerException?.Message ?? "Argument is null";
-                        }
-                        else
-                        {
-                            message = contextFeatures.Error.Message ?? contextFeatures.Error.InnerException?.Message ?? "Bad Request";
-                        }
-                        context.Response.StatusCode = statusCode;
-                        logger.LogError(message);
-                        await context.Response.WriteAsync(JsonSerializer.Serialize(new
-                        {
-                            StatusCode = statusCode,
-                            ErrorMessage = message,
-                            Title = "Gozlenilmez xeta bash verdi"
-                        }));
+                        statusCode = customEx.StatusCode;
+                        message = customEx.ErrorMessage;
                     }
-                });
+                    else if (contextFeatures.Error is InvalidOperationException)
+                    {
+                        statusCode = 400;
+                        message = contextFeatures.Error.Message ?? contextFeatures.Error.InnerException?.Message ??"Bad Request";
+                    }
+                    else if (contextFeatures.Error is ArgumentNullException)
+                    {
+                        statusCode = 400;
+                        message = contextFeatures.Error.Message ?? contextFeatures.Error.InnerException?.Message ?? "Argument is null";
+                    }
+                    else
+                    {
+                        message = contextFeatures.Error.Message ?? contextFeatures.Error.InnerException?.Message ?? "Bad Request";
+                    }
+                    context.Response.StatusCode = statusCode;
+                    logger.LogError(message);
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(new
+                    {
+                        StatusCode = statusCode,
+                        ErrorMessage = message,
+                        Title = "Gozlenilmez xeta bash verdi"
+                    }));
+                }
             });
-        }
+        });
     }
 }
